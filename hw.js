75038c6028889672a7a03372fdb568d3baa9321b
IulikTKW <inicula1989uk@gmail.com>
2024-10-02 21:05:39 +0300

    1

---
+++ hw.js
@@ -0,0 +1,19 @@
+const number = 1;
+const str = 'string';
+const bool = false;
+const Null = null;
+const undief = undefined;
+const symb = Symbol();
+const bigInt = BigInt(9);
+const obj = {};
+
+// console.log(typeof number)
+// console.log(typeof str)
+// console.log(typeof bool)
+// console.log(typeof bigInt)
+// console.log(typeof Null)
+// console.log(typeof undief)
+// console.log(typeof symb)
+// console.log(typeof obj)
+
+console.log('test' !== 'Test')
---
+++ index.js
@@ -0,0 +1,78 @@
+const primulCuvantRestuToate = {};
+const SNAKE_CASE = 'SNAKE CASE';
+const Capital = 'test';
+const kebab_case = 'denumirea claselor';
+
+const NUME = 'Iulian'
+
+// console.log('My name' + ': ' + 'Iulian')
+
+// console.log(`My name is: ${3 + 3} and i use ${SNAKE_CASE}`)
+
+
+// const str = '7T'
+
+// console.log(typeof +str, +str)
+// console.log(typeof Number(str), Number(str))
+// console.log(3+ +str)
+
+// console.log('don\'t go there')
+
+// const num = 1;
+
+// console.log(typeof  num.toString().charAt())
+
+// const str = 'str';
+
+// const condition = false;
+
+// if(typeof str === 'number' ) {
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// == ===, > < != !==, !, >= <=
+
+
+// if(3 > 1 ) {
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// if(3 >= 1 ) {
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// if(3 !== 1 ) {
+//     console.log('pass')
+// } else if (3 > 2) {
+//     console.log('not pass')
+// }
+
+// const condition1 = true;
+// const condition2 = false;
+
+// if ( condition1 || condition2 || condition2 || condition2 ) {
+//     console.log('pass')
+// }
+
+// const defaultName = 'User';
+// const userName = '';
+
+// const usedName = userName || defaultName;
+
+// console.log(usedName)
+
+// console.log(true && false && true)
+
+const condition = false;
+const condition2 = false;
+const condition3 = false;
+
+const ternary = condition ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+console.log(ternary)
